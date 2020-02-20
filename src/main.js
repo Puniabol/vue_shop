1a7a28565626590f23e25d9560ea45a2dcf00e99
chockitlee <942020960@qq.com>
2020-02-20 20:54:53 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = '' // 无接口地址（http://……）
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
-
 const router = new VueRouter({
-    routes
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        { path: '/home', component: Home },
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next()
+
+    // 获取token
+    // const tokenStr = window.sessionStorage.getItem('token')
+    // 无token，定义替代
+    const tokenStr = true
+
+    if (!tokenStr) return next('/login')
+    next()
 })
 
 export default router
